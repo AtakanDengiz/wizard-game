5c4a4c860a1a7304d1fbb2bdd8ce51ee67f6f749
AtakanDengiz <atakanofficial@hotmail.com>
2022-05-21 16:55:07 +0300

    Visual assets are implemented. Made some skill throw adjustments.

--- screen_scrolling.py
+++ screen_scrolling.py
@@ -34,9 +34,9 @@ grenade = False
 grenade_thrown = False
 
 # load images
-pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
-pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
-mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
+#pine1_img = pygame.image.load('img/Background/pine1.png').convert_alpha()
+#pine2_img = pygame.image.load('img/Background/pine2.png').convert_alpha()
+#mountain_img = pygame.image.load('img/Background/mountain.png').convert_alpha()
 sky_img = pygame.image.load('img/Background/sky_cloud.png').convert_alpha()
 # store tiles in a list
 img_list = []
@@ -59,7 +59,7 @@ item_boxes = {
 }
 
 # define colours
-BG = (144, 201, 120)
+BG = (6,53,82)
 RED = (255, 0, 0)
 WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
@@ -78,9 +78,9 @@ def draw_bg():
     width = sky_img.get_width()
     for x in range(5):
         screen.blit(sky_img, ((x * width) - bg_scroll * 0.5, 0))
-        screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
-        screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
-        screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
+        #screen.blit(mountain_img, ((x * width) - bg_scroll * 0.6, SCREEN_HEIGHT - mountain_img.get_height() - 300))
+        #screen.blit(pine1_img, ((x * width) - bg_scroll * 0.7, SCREEN_HEIGHT - pine1_img.get_height() - 150))
+        #screen.blit(pine2_img, ((x * width) - bg_scroll * 0.8, SCREEN_HEIGHT - pine2_img.get_height()))
 
 
 class Soldier(pygame.sprite.Sprite):
