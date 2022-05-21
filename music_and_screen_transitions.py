bb740e60529a5a8192167beb5db1c2cf231a976c
AtakanDengiz <atakanofficial@hotmail.com>
2022-05-21 17:02:26 +0300

    Game name changed.

--- music_and_screen_transitions.py
+++ music_and_screen_transitions.py
@@ -12,7 +12,7 @@ SCREEN_WIDTH = 800
 SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-pygame.display.set_caption('Shooter')
+pygame.display.set_caption('Destiny of Wizard')
 
 # set framerate
 clock = pygame.time.Clock()
